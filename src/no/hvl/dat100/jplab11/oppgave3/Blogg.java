d15980aa17f658f062641a43a1b93f4ec60cf258
Tamer <Tamer@10.0.0.5>
2022-11-15 21:55:24 +0100

    Hele innlevering - Tamer

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -2,55 +2,133 @@ package no.hvl.dat100.jplab11.oppgave3;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave1.*;
+import no.hvl.dat100.jplab11.oppgave2.Bilde;
+import no.hvl.dat100.jplab11.oppgave2.Tekst;
 
 public class Blogg {
 
 	// TODO: objektvariable 
+	private Innlegg [] innleggtabell;
+	private int nesteledig ;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		
+		innleggtabell = new Innlegg [20];
+		
+		nesteledig = 0 ;
+		
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		
+		innleggtabell = new Innlegg [lengde];
+
+		nesteledig = 0 ; 
+		
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+	
+		return nesteledig  ;
+				
+		
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		
+		return innleggtabell;
+		
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		for ( int i = 0 ; i < nesteledig; i++) {
+			
+			if (innleggtabell[i].erLik(innlegg)) {
+				return i;
+			}
+			
+		}
+		return -1;
+		
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		int pos = 0;
+		
+		boolean funnet = false;
+		
+		while (pos < nesteledig && !funnet )
+			if (innleggtabell[pos].erLik(innlegg)) {
+				funnet = true;
+			}
+			else 
+				pos++;
+		
+	
+		return funnet;
+		
+		
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		int pos =0;
+		boolean ok = false;
+		//boolean ok = finnInnlegg(innleggtabell[pos])!=-1;
+		while (pos < nesteledig && !ok) {
+		if ( nesteledig < innleggtabell.length) {
+			ok = true;
+			
+		}else pos++;
+		}
+		if (ok)
+			return true;
+		else
+			return false;
+		
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		int pos =0;
+		boolean ny = false;
+		//boolean ny = 
+		
+		if (ny = finnInnlegg(innlegg) == -1 && nesteledig < innleggtabell.length) {
+			
+			innleggtabell[nesteledig] = innlegg;
+			nesteledig++;
+			ny = true;
+			
+		}
+			return ny;
+	
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		String svar = "";
+		for (int i = 0; i < nesteledig; i++) {
+			svar += innleggtabell[i].toString() ;
+		}
+		svar = getAntall() +"\n"+ svar;
+		return svar;
+		
+		
 	}
 
 	// valgfrie oppgaver nedenfor
 	
 	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		Innlegg [] nyTab = new Innlegg [innleggtabell.length *2];
+		
+		for ( int pos = 0 ; pos < innleggtabell.length; pos ++ ) {
+			nyTab[pos]= innleggtabell[pos];
+			
+		}
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 	
 	public boolean leggTilUtvid(Innlegg innlegg) {
--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,15 +1,34 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
-import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+
+
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
+	
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+	
+		FileWriter fileWriter;
+		try {
+			fileWriter = new FileWriter(mappe + filnavn);
+			fileWriter.append(samling.toString());
+			fileWriter.close();
+			
+			return true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+	
 	}
 }
